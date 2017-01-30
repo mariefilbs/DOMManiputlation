5ab6a2f8c42e58b5ec5e5f58c55c9180c2214887
Marie Filbey <patriciamariefilbey@gmail.com>
2017-01-30 13:59:59 -0500

    Finished easy mode calculator.

---
+++ main.js
@@ -0,0 +1,22 @@
+//Define answer:
+var answer=document.getElementById("answer");
+
+//Define calculate button:
+var calcButton=document.getElementById("calculate-button");
+
+var calculate = function () {
+  //Define the two input boxes
+  var input1=document.getElementById("value-one");
+  var input2=document.getElementById("value-two");
+
+  //Math Time!
+  var x = Number(input1.value);
+  var y = Number(input2.value);
+    var sum = x + y;
+
+    //Answer on the page
+    answer.innerHTML = sum;
+};
+
+//Click Event:
+calcButton.addEventListener("click", calculate);
